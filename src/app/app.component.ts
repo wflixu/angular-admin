14ef5e9ff0bbdd1156b4532ec6ae7c4d3f30a5c7
wflixu <wflixu@yeah.net>
2019-06-15 10:02:43 +0800

    warn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles:[
-    `:host ::ng-deep .logo {
-    width: 120px;
-    height: 31px;
-    background: #333;
-    border-radius: 6px;
-    margin: 16px 28px 16px 0;
-    float: left;
-  }`
-],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  size = 'large';
+  openlink(): void {
+    const os = 'android/ios';
+     if (os === 'android'){
+         //
+         location.href = 'andriod 跳转链接 ';
+     }else if (os === 'ios')(
+       location.href = 'ios 跳转链接 ';
+     )else{
+      location.href = '其他链接 跳转链接 ';
+     }
+  }
 }
+
+
